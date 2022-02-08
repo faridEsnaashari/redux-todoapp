6c6b364682217410a37d7d9ced33171dfe25239b
faridEsnaashari <faride13578642@gmail.com>
2022-02-08 17:13:03 +0330

    remove function implementation has been moved to App component

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,16 +1,13 @@
 import React from "react";
 import store from "../redux/store";
-const Card = ({ text, id }) => {
-  const remove = (id) => {
-    store.dispatch({ type: "REMOVE_ITEM", payload: id });
-  };
+const Card = ({ text, id, onRemove }) => {
   
   return (
     <div className="card my-3">
       <div className="card-body">
         <h5 className="card-title">{text}</h5>
 
-        <a onClick={() => remove(id)} className="btn btn-danger">
+        <a onClick={() => onRemove(id)} className="btn btn-danger">
           Remove
         </a>
       
