ae18d90d19e6e3f276b6140d88b452c06bf8a85d
faridEsnaashari <faride13578642@gmail.com>
2022-02-08 17:14:31 +0330

    remveItem and addItem actions were added

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -2,3 +2,6 @@ export const ADD_ITEM = "ADD_ITEM"
 
 export const REMOVE_ITEM = "REMOVE_ITEM"
 
+export const removeItem = id => ({ type: REMOVE_ITEM, payload: id });
+
+export const addItem = text => ({ type: ADD_ITEM , payload : text });
